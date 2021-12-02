75286a7de5efc0f5f9a5340e37df6f4219064fdf
Rida <rridane@rr-dev.com>
2021-12-02 14:32:18 +0100

    add tmux plug

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-sudo curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sudo apt install tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 cp ./.vimrc ~
 cp ./.tmux.conf ~
