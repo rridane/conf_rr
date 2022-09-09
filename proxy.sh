1b5eeb5a908551078c69107bfa88ea882eca627b
rridane <rridane@localhost.localdomain>
2022-09-09 13:20:18 +0200

    update proxy script

--- proxy.sh
+++ proxy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-source /etc/environment
+source /etc/profile
 
 if [ -z $http_proxy ]
 then
 
-  sed -r -i "/^(proxy|http_proxy|https_proxy)=$/d" /etc/environment
+  sudo sed -r -i "/^(proxy|http_proxy|https_proxy)=$/d" /etc/profile
 
   echo "set proxy"
-  echo 'Acquire::http::Proxy "http://CptLinuxWeb:LinuxWeb@10.33.50.30:3128";' >> /etc/apt.conf
-  echo 'Acquire::https::Proxy "http://CptLinuxWeb:LinuxWeb@10.33.50.30:3128";' >> /etc/apt.conf
+  sudo echo 'Acquire::http::Proxy "http://CptLinuxWeb:LinuxWeb@10.33.50.30:3128";' >> /etc/apt.conf
+  sudo echo 'Acquire::https::Proxy "http://CptLinuxWeb:LinuxWeb@10.33.50.30:3128";' >> /etc/apt.conf
 
-  echo "proxy=\"http://CptLinuxWeb:LinuxWeb@10.33.50.30:3128\"" >> /etc/environment
-  echo "https_proxy=\"http://CptLinuxWeb:LinuxWeb@10.33.50.30:3128\"" >> /etc/environment
-  echo "http_proxy=\"http://CptLinuxWeb:LinuxWeb@10.33.50.30:3128"\" >> /etc/environment
+  sudo echo "export proxy=\"http://CptLinuxWeb:LinuxWeb@10.33.50.30:3128\"" >> /etc/profile
+  sudo echo "export https_proxy=\"http://CptLinuxWeb:LinuxWeb@10.33.50.30:3128\"" >> /etc/profile
+  sudo echo "export http_proxy=\"http://CptLinuxWeb:LinuxWeb@10.33.50.30:3128"\" >> /etc/profile
   
-  source /etc/environment
+  source /etc/profile
 
   git config --global http.proxy http://CptLinuxWeb:LinuxWeb@10.33.50.30:3128
   git config --global https.proxy http://CptLinuxWeb:LinuxWeb@10.33.50.30:3128
@@ -30,8 +30,9 @@ else
   unset https_proxy
   unset proxy
 
-  sed -r -i "s/^(proxy|http_proxy|https_proxy)(.*)/\1=""/g" /etc/environment
+  sudo sed -r -i "s/^export\ (proxy|http_proxy|https_proxy)(.*)/\1=""/g" /etc/profile
+  sudo echo "" > /etc/apt.conf
 
-  source /etc/environment
+  source /etc/profile
 
 fi
